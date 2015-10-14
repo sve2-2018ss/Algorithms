48b2b415cb4547551d42186f97b18f1b0a349908
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-10-14 12:14:26 +0300

    Priority Queues

---
+++ Priority Queues/Priority Queues/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priority_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the top M lines in the input stream.
+            Console.Write("Write Lenth : ");
+            int M = int.Parse(Console.ReadLine());
+            Console.Write("Write Del max(True) or min(False) : ");
+            bool b = false;
+            bool.TryParse(Console.ReadLine(),out b);
+
+            PriorityQueue<int> pq = new PriorityQueue<int>(M,b);
+
+            Console.WriteLine("Insert Queue : ");
+            for(int i=0;i<M;i++)
+                pq.Insert(int.Parse(Console.ReadLine()));
+
+            Stack<int> stack = new Stack<int>();
+
+            while (!pq.isEmpty())
+                stack.push(pq.Del());
+
+            Console.WriteLine("\n\t OutPut Stack");
+            while(!stack.isEmpty())
+                Console.WriteLine(stack.pull());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priority Queues/Priority Queues/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Priority_Queues
+{
+    public class Stack<T> where T : IComparable<T>
+    {
+        private List<T> stack;
+
+        public Stack()
+        {
+            stack=new List<T>();
+        }
+
+        public bool isEmpty()
+        {
+            return stack.Count == 0;
+        } 
+
+        public void push(T p)
+        {
+            stack.Add(p);
+        }
+
+        public T pull()
+        {
+            T temp = stack.Last();
+            stack.Remove(temp);
+            return temp;
+        }
+
+    }
+}
