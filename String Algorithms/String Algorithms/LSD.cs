eec862dea794f6a77d039d86f76e3fa59f1eac55
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-10-17 17:17:19 +0300

    LSD string sort

---
+++ String Algorithms/String Algorithms/LSD.cs
@@ -0,0 +1,24 @@
+﻿namespace String_Algorithms
+{
+    public static class LSD
+    {
+        public static void sort(string[] a, int W)
+        { // Sort a[] on leading W characters.
+            int N = a.Length;
+            int R = 256;
+            string[] aux = new string[N];
+            for (int d = W - 1; d >= 0; d--)
+            { // Sort by key-indexed counting on dth char.
+                int[] count = new int[R + 1]; // Compute frequency counts.
+                for (int i = 0; i < N; i++)
+                    count[a[i][d] + 1]++;
+                for (int r = 0; r < R; r++) // Transform counts to indices.
+                    count[r + 1] += count[r];
+                for (int i = 0; i < N; i++) // Distribute.
+                    aux[count[a[i][d]]++] = a[i];
+                for (int i = 0; i < N; i++) // Copy back.
+                    a[i] = aux[i];
+            }
+        }
+    }
+}
---
+++ String Algorithms/String Algorithms/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs=new string[]
+            {
+                "4PGC938",
+                "2IYE230",
+                "3CIO720",
+                "1ICK750",
+                "1OHV845",
+                "4JZY524",
+                "1ICK750",
+                "3CIO720",
+                "1OHV845",
+                "1OHV845",
+                "2RLA629",
+                "2RLA629",
+                "3ATW723"
+            };
+
+            strs.Show();
+            Console.WriteLine();
+
+            LSD.sort(strs,7);
+
+            strs.Show();
+            Console.ReadKey();
+
+        }
+    }
+
+    public static class Helpers
+    {
+        public static void Show(this string[] strs)
+        {
+            foreach (var str in strs)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
