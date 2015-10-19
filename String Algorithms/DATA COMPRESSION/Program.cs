45e44eee1925ff09ceef270594000b1cea8722c7
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-10-19 12:00:33 +0300

    Huffman Compression Algorithm

---
+++ String Algorithms/DATA COMPRESSION/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_COMPRESSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INput String for Encoding & Decoding: ");
+            string input = Console.ReadLine();
+            HuffmanTree huffmanTree = new HuffmanTree();
+
+            // Build the Huffman tree
+            huffmanTree.Build(input);
+
+            // Encode
+            BitArray encoded = huffmanTree.Encode(input);
+
+            Console.Write("Encoded: ");
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+            // Decode
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.WriteLine("Decoded: " + decoded);
+
+            Console.ReadKey();
+        }
+    }
+}
